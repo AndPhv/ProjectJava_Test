d80a5a7c48aaa8217907f36570347089dd6bdfc7
AndPhv <puhov2001@gmail.com>
2022-03-20 12:09:41 +0300

    Создание метода для вывода на экран списка достопримечательностей и подсчета среднего значения времени

---
+++ src/com/company/OptimalRoute.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class OptimalRoute extends ListAttractions {
+    float[] listFloatTime = new float[list.length];
+    float[] listFloatImportance = new float[list.length];
+    String[] listStringSight = new String[list.length];
+
+    double averageTime = 0;
+    double sumTime = 0;
+    int count = 0;
+
+    public void printListAttraction() {
+        for (int i = 0; i < list.length; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (j == 1)
+                {
+                    listFloatTime[i] = Float.parseFloat(list[i][j]);
+                    sumTime += listFloatTime[i];
+                }
+                else if (j == 2)
+                    listFloatImportance[i] = Float.parseFloat(list[i][j]);
+                System.out.print(list[i][j] + "\t\t");
+            }
+            System.out.println();
+        }
+        averageTime = Math.rint(sumTime / listFloatTime.length);
+    }
+}
